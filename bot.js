160845908f350859d2abc03498f9e79241c7e51b
Gijix <orlando.srnic@gmail.com>
2021-03-02 19:01:54 +0100

    reaction collector work

--- bot.js
+++ bot.js
@@ -1,68 +1,76 @@
-require('dotenv').config();
-const Discord = require('discord.js');
+require("dotenv").config();
+const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-const prefix = '!';
+const prefix = "!";
 
-bot.on('ready', () => {
-  console.info('Orlando bot has started');
-  bot.user.setStatus('available');
-  bot.user.setPresence({ status: 'online', activity: { name: '!<commandname>', type: 'WATCHING' } });
+bot.on("ready", () => {
+  console.info("Orlando bot has started");
+  bot.user.setStatus("available");
+  bot.user.setPresence({
+    status: "online",
+    activity: { name: "!<commandname>", type: "WATCHING" },
+  });
 });
 
-bot.on('message', async (message) => {
+bot.on("message", async (message) => {
   const guild = message.guild;
+  const firstMentionGuildMember = message.mentions.members.first();
+  console.log("STEP 1");
 
-  if (firstMentionGuildMember && message.content.startsWith(prefix + 'matchwith')) {
-    message.react('✅');
-    message.react('❌');
+  if (
+    firstMentionGuildMember &&
+    message.content.startsWith(prefix + "matchwith")
+  ) {
+    message.react("✅");
+    message.react("❌");
+    const filter = (reaction, user) => reaction.emoji.name === "✅";
 
-    try {
-      const reactions = await message.awaitReactions(
-        (reaction, user) => user.id === firstMentionGuildMember.user.id && reaction.emoji.name === '✅',
-        {
-          max: 30,
-        }
-      );
-      console.log(reactions);
-      if (reactions.first().emoji.name === '✅') {
+    let collector = message.createReactionCollector(filter, { max: 30 });
+    collector.on("collect", (reaction, user) => {
+      if (user.id === firstMentionGuildMember.user.id) {
+        console.log("It worked");
         const firstRoleName = message.author.username;
         const secondRoleName = firstMentionGuildMember.user.username;
-        console.log(`inside condition ${firstRoleName} ${secondRoleName}`);
-        // guild.roles.create({
-        //   data: {
-        //     name: firstRoleName,
-        //     color: 'BLUE',
-        //   },
-        // });
-        // guild.roles.create({
-        //   data: {
-        //     name: secondRoleName,
-        //     color: 'RED',
-        //   },
-        // });
+        guild.roles.create({
+          data: {
+            name: firstRoleName,
+            color: "BLUE",
+          },
+        });
 
-        console.log('it worked');
+        guild.roles.create({
+          data: {
+            name: secondRoleName,
+            color: "RED",
+          },
+        });
+        // let role1 = guild.roles.cache.array().find((r) => r.name === firstRoleName);
+        // let role2 = guild.roles.cache.array().find((r) => r.name === secondRoleName);
+        // console.log(role1);
+        // console.log(role2)
+
+        // message.member.roles.add(role2);
+        // message.mentions.members.first().roles.add(role1);
+        // message.delete();
       }
-    } catch (error) {
-      console.log(error);
-    }
+    });
+    // const messageArr = message.content.split(' ');
+    // const amount = parseInt(messageArr[1]);
+    // console.log(amount);
+    // console.log(message.channel);
+    // if (message.content.startsWith(prefix + 'clear') && messageArr.length === 2) {
+    //   message.channel.bulkDelete(amount).catch(console.error);
+    // }
   }
-  // const messageArr = message.content.split(' ');
-  // const amount = parseInt(messageArr[1]);
-  // console.log(amount);
-  // console.log(message.channel);
-  // if (message.content.startsWith(prefix + 'clear') && messageArr.length === 2) {
-  //   message.channel.bulkDelete(amount).catch(console.error);
-  // }
 });
 
-process.on('unhandledRejection', (error) => {
-  throw new Error('Unhandled promise rejection:', error);
+process.on("unhandledRejection", (error) => {
+  throw new Error("Unhandled promise rejection:", error);
 });
 
-bot.on('error', (error) => {
-  console.info('The websocket connection encountered an error:', error);
+bot.on("error", (error) => {
+  console.info("The websocket connection encountered an error:", error);
 });
 
 bot.login(process.env.BOT_TOKEN);
