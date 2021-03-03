3ca0ce0c4937ed1c79e697aa4fafdd56a816b21a
Gijix <orlando.srnic@gmail.com>
2021-03-03 13:28:55 +0100

    add condition

--- bot.js
+++ bot.js
@@ -14,64 +14,62 @@ bot.on("ready", () => {
 });
 
 bot.on("message", async (message) => {
-  const guild = message.guild;
-  const firstMentionGuildMember = message.mentions.members.first();
-  console.log("STEP 1");
+  const { guild } = message;
+  if (message.mentions.members) {
+    const firstMentionGuildMember = message.mentions.members.first();
 
-  if (
-    firstMentionGuildMember &&
-    message.content.startsWith(prefix + "matchwith")
-  ) {
-    message.react("✅");
-    message.react("❌");
-    const filter = (reaction, user) => reaction.emoji.name === "✅";
+    if (
+      firstMentionGuildMember &&
+      message.content.startsWith(prefix + "matchwith")
+    ) {
+      message.react("✅");
+      message.react("❌");
+      const filterA = (reaction, user) => reaction.emoji.name === "✅";
+      const filterB = (reaction, user) => reaction.emoji.name === "❌";
 
-    let collector = message.createReactionCollector(filter, { max: 30 });
-    collector.on("collect", (reaction, user) => {
-      if (user.id === firstMentionGuildMember.user.id) {
-        console.log("It worked");
-        const firstRoleName = message.author.username;
-        const secondRoleName = firstMentionGuildMember.user.username;
-        guild.roles.create({
-          data: {
-            name: firstRoleName,
-            color: "BLUE",
-          },
-        }).then;
-
-        guild.roles.create({
-          data: {
-            name: secondRoleName,
-            color: "RED",
-          },
-        });
-        setTimeout(function () {
-          
-            const role1 = guild.roles.cache.filter((r) => r.name === firstRoleName)
-            const role2 = guild.roles.cache.filter((r) => r.name === secondRoleName)
-            message.member.roles.add(role2)
-            firstMentionGuildMember.roles.add(role1)
-
-
-          ;
-        }, 1000);
-        // let role1 = guild.roles.cache.array().find((r) => r.name === firstRoleName);
-        // let role2 = guild.roles.cache.array().find((r) => r.name === secondRoleName);
-        // console.log(role1);
-        // console.log(role2)
-
-        // message.member.roles.add(role2);
-        // message.mentions.members.first().roles.add(role1);
-        // message.delete();
-      }
-    });
-    // const messageArr = message.content.split(' ');
-    // const amount = parseInt(messageArr[1]);
-    // console.log(amount);
-    // console.log(message.channel);
-    // if (message.content.startsWith(prefix + 'clear') && messageArr.length === 2) {
-    //   message.channel.bulkDelete(amount).catch(console.error);
-    // }
+      let collectorA = message.createReactionCollector(filterA, { max: 30 });
+      let collectorB = message.createReactionCollector(filterA, { max: 30 });
+      collectorA.on("collect", (reaction, user) => {
+        if (user.id === firstMentionGuildMember.user.id) {
+          const firstRoleName = message.author.username;
+          const secondRoleName = firstMentionGuildMember.user.username;
+          const role1 = guild.roles.cache.find((r) => r.name === firstRoleName);
+          const role2 = guild.roles.cache.find(
+            (r) => r.name === secondRoleName
+          );
+          if (role1 === undefined) {
+            console.log("LAST STEP");
+            guild.roles.create({
+              data: {
+                name: firstRoleName,
+                color: "BLUE",
+              },
+            });
+          }
+          if (role2 === undefined) {
+            console.log("LAST STEP 2");
+            guild.roles.create({
+              data: {
+                name: secondRoleName,
+                color: "RED",
+              },
+            });
+          }
+          setTimeout(function () {
+            message.member.roles.add(role2);
+            firstMentionGuildMember.roles.add(role1);
+          }, 1000);
+        }
+      });
+      collectorB.on;
+      // const messageArr = message.content.split(' ');
+      // const amount = parseInt(messageArr[1]);
+      // console.log(amount);
+      // console.log(message.channel);
+      // if (message.content.startsWith(prefix + 'clear') && messageArr.length === 2) {
+      //   message.channel.bulkDelete(amount).catch(console.error);
+      // }
+    }
   }
 });
 
